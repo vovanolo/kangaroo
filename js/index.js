2374aaad249ecbcabd4fb67786cd3ffc4d7b0f38
Vovchik4 <ch.vova321@gmail.com>
2020-08-26 15:03:43 +0300

    Test

--- js/index.js
+++ js/index.js
@@ -4,24 +4,23 @@ $(document).ready(function () {
   });
 });
 
-const navbar = document.querySelector('.navbar');
-const navbarToggler = document.querySelector('.navbar-toggler');
-const navbarCollapse = document.querySelector('.navbar-collapse');
+const navbar = document.querySelector(".navbar");
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarCollapse = document.querySelector(".navbar-collapse");
 
-window.addEventListener('scroll', (e) => {
+window.addEventListener("scroll", (e) => {
   const scrollY = e.currentTarget.scrollY;
   if (scrollY > 100) {
-    navbar.classList.add('bg-light');
-  }
-  else {
-    navbar.classList.remove('bg-light');
+    navbar.classList.add("bg-light");
+  } else {
+    navbar.classList.remove("bg-light");
   }
 });
 
-$('#navbarSupportedContent').on('show.bs.collapse', () => {
-  navbar.classList.add('bg-light');
+$("#navbarSupportedContent").on("show.bs.collapse", () => {
+  navbar.classList.add("bg-light");
 });
 
-$('#navbarSupportedContent').on('hide.bs.collapse', () => {
-  navbar.classList.remove('bg-light');
+$("#navbarSupportedContent").on("hide.bs.collapse", () => {
+  navbar.classList.remove("bg-light");
 });
