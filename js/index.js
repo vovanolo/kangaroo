40bb515a4e2ef100747535359a5266ee9fa9555c
vovanolo <vovchik.student221@gmail.com>
2020-08-26 12:51:37 +0300

    new menu

--- js/index.js
+++ js/index.js
@@ -3,3 +3,25 @@ $(document).ready(function () {
     interval: 100000,
   });
 });
+
+const navbar = document.querySelector('.navbar');
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+
+window.addEventListener('scroll', (e) => {
+  const scrollY = e.currentTarget.scrollY;
+  if (scrollY > 100) {
+    navbar.classList.add('bg-light');
+  }
+  else {
+    navbar.classList.remove('bg-light');
+  }
+});
+
+$('#navbarSupportedContent').on('show.bs.collapse', () => {
+  navbar.classList.add('bg-light');
+});
+
+$('#navbarSupportedContent').on('hide.bs.collapse', () => {
+  navbar.classList.remove('bg-light');
+});
